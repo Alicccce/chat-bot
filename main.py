b44f2adfe9a4ddbd9e8c5fc8233c5a1414e24551
aliis <aliissa.mrak@mail.ru>
2024-04-08 20:25:27 +0300

    клава1

--- main.py
+++ main.py
@@ -1,69 +1,53 @@
 import logging
 from telegram.ext import Application, MessageHandler, filters
 from telegram.ext import CommandHandler
+from telegram import ReplyKeyboardMarkup
 
 
 BOT_TOKEN = '7131922662:AAFHddt6nU3S-olQtsV7g0dd_nHaMSNafv4'
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
 logger = logging.getLogger(__name__)
-TIMER = 5
+reply_keyboard = [['/wrong', '/ready_plan'],
+                  ['/person_plan', '/remind']]
+markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
 
 
 async def start(update, context):
     user = update.effective_user
-    await update.message.reply_html(
-        rf"Привет {user.mention_html()}! Я эхо-бот. Напишите мне что-нибудь, и я пришлю это назад!",)
-
-
-async def echo(update, context):
-    await update.message.reply_text(f'Я получил сообщение {update.message.text}')
+    await update.message.reply_html(rf"Привет, {user.mention_html()}! Я чат-бот Гриша, пришёл к вам с планеты Школяриус, чтобы помочь сдать экзамен по профильной математике! С чего начнём?",
+                                    reply_markup=markup)
 
 
 async def help_command(update, context):
-    await update.message.reply_text("Я пока не умею помогать... Я только ваше эхо.")
+    await update.message.reply_text("Я пока не умею помогать...")
 
 
-async def task(context):
-    await context.bot.send_message(context.job.chat_id, text=f'КУКУ! 5c. прошли!')
+async def remind(update, context):
+    await update.message.reply_text('Такое сделаем! r')
 
 
-async def unset(update, context):
-    chat_id = update.message.chat_id
-    job_removed = remove_job_if_exists(str(chat_id), context)
-    text = 'Таймер отменен!' if job_removed else 'У вас нет активных таймеров'
-    await update.message.reply_text(text)
+async def person_plan(update, context):
+    await update.message.reply_text('Такое сделаем! pp')
 
 
-def remove_job_if_exists(name, context):
-    current_jobs = context.job_queue.get_jobs_by_name(name)
-    if not current_jobs:
-        return False
-    for job in current_jobs:
-        job.schedule_removal()
-    return True
+async def ready_plan(update, context):
+    await update.message.reply_text('Такое сделаем! rp')
 
 
-async def set_timer(update, context):
-    chat_id = update.effective_message.chat_id
-    job_removed = remove_job_if_exists(str(chat_id), context)
-    context.job_queue.run_once(task, TIMER, chat_id=chat_id, name=str(chat_id), data=TIMER)
-
-    text = f'Вернусь через 5 с.!'
-    if job_removed:
-        text += ' Старая задача удалена.'
-    await update.effective_message.reply_text(text)
+async def wrong(update, context):
+    await update.message.reply_text('Такое сделаем! w')
 
 
 def main():
     application = Application.builder().token(BOT_TOKEN).build()
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
-    application.add_handler(CommandHandler("set", set_timer))
-    application.add_handler(CommandHandler("unset", unset))
+    application.add_handler(CommandHandler("remind", remind))
+    application.add_handler(CommandHandler("person_plan", person_plan))
+    application.add_handler(CommandHandler("ready_plan", ready_plan))
+    application.add_handler(CommandHandler("wrong", wrong))
 
-    text_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, echo)
-    application.add_handler(text_handler)
     application.run_polling()
 
 
